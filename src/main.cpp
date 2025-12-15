9a0f85c08b616af2c294a83113197fa1cd12b39d
“YXH” <q105277951@gmail.com>
2025-12-15 19:48:16 +0800

    添加格式化文件

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include <QApplication>
 #include <QLabel>
 
-int main(int argc, char **argv) {
-  QApplication app(argc, argv);
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
 
-  QLabel label("Qt6 minimal template");
-  label.resize(320, 120);
-  label.show();
+    QLabel label("Qt6 minimal template");
+    label.resize(320, 120);
+    label.show();
 
-  return app.exec();
+    return app.exec();
 }
